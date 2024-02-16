8b1ff0657c883c02c93dc6c237e5fe27f32a6d72
Arsen Chernov <arsenchernov02@gmail.com>
2024-02-16 14:26:27 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Header from "./Components/Header/Header";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        smth
+      </main>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='bg-primary d-flex justify-content-between container-fluid'>
+      <h1 className='text-light'>Static pages</h1>
+      <nav>
+        <ul className='list-group d-flex flex-row'>
+          <li className='navItem list-group-item bg-primary text-light border-0'>
+            <NavLink to="/" end className={({ isActive }) => isActive ? 'active' : undefined}>Home</NavLink>
+          </li>
+          <li className='navItem list-group-item bg-primary border-0'>
+            <NavLink to="/about-us" className={({ isActive }) => isActive ? 'active' : undefined}>About Us</NavLink>
+          </li>
+          <li className='navItem list-group-item bg-primary border-0'>
+            <NavLink to="/contacts" className={({ isActive }) => isActive ? 'active' : undefined}>Contacts</NavLink>
+          </li>
+          <li className='navItem list-group-item bg-primary border-0'>
+            <NavLink to="/divisions" className={({ isActive }) => isActive ? 'active' : undefined}>Divisions</NavLink>
+          </li>
+          <li className='navItem list-group-item bg-primary border-0'>
+            <NavLink to="/another-page" className={({ isActive }) => isActive ? 'active' : undefined}>Another Page</NavLink>
+          </li>
+          <li className='navItem list-group-item bg-primary border-0'>
+            <NavLink to="/admin" className={({ isActive }) => isActive ? 'active' : undefined}>Admin</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
